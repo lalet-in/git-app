ed7a657932c8016a34eb30b6bcfc702a722e1496
lalet-in <artem@lalet.in>
2020-11-13 00:19:06 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const  Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://cdn.fishki.net/upload/post/201502/17/1431447/928051198014b5ac3055adfbd1fb99d8.jpg' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const  Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://luxfon.com/download.php?file=201203/800x600/luxfon.com-4786.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
